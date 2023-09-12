145e29ecd96979108799f775a85ef07505f34e06
suraj-k-s <surajks28101999@gmail.com>
2023-09-12 19:07:53 +0530

    Config Updated

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,15 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 //Paste Firebase COnnection Code Here 
-const firebaseConfig = {};
+const firebaseConfig = {
+    apiKey: "AIzaSyAzYnOsHAP4xZBeXKnb2KIqEeBYWHa5CW4",
+    authDomain: "soo-chat-2023.firebaseapp.com",
+    projectId: "soo-chat-2023",
+    storageBucket: "soo-chat-2023.appspot.com",
+    messagingSenderId: "935907579333",
+    appId: "1:935907579333:web:719a67dead1259443be7ac",
+    measurementId: "G-CGY56CTTN1"
+  };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
